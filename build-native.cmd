790fb57f1cca5083db32bf07f814a5e11044e9a7
Rofli Souza <rofli@kakunyn.com>
2026-04-20 00:11:52 -0300

    Fix Windows library build script paths

--- build-native.cmd
+++ build-native.cmd
@@ -1,29 +1,29 @@
 @setlocal
 @echo off
 
-set BUILD_CONFIG=Debug
-set BUILD_ARCH=x64
-set BUILD_CMAKE_GENERATOR_PLATFORM=x64
-set MSVC_RUNTIME="MultiThreadedDebug"
-set BUILD_LIB=cimgui
+set "BUILD_CONFIG=Debug"
+set "BUILD_ARCH=x64"
+set "BUILD_CMAKE_GENERATOR_PLATFORM=x64"
+set "MSVC_RUNTIME=MultiThreadedDebug"
+set "BUILD_LIB=cimgui"
 
 :ArgLoop
 if [%1] == [] goto Build
-if /i [%1] == [Release] (set BUILD_CONFIG=Release && set MSVC_RUNTIME=MultiThreaded && shift & goto ArgLoop)
-if /i [%1] == [Debug] (set BUILD_CONFIG=Debug && set MSVC_RUNTIME=MultiThreadedDebug && shift & goto ArgLoop)
-if /i [%1] == [x64] (set BUILD_ARCH=x64 && shift & goto ArgLoop)
-if /i [%1] == [ARM64] (set BUILD_ARCH=ARM64 && set BUILD_CMAKE_GENERATOR_PLATFORM=ARM64 && shift & goto ArgLoop)
-if /i [%1] == [ARM] (set BUILD_ARCH=ARM && set BUILD_CMAKE_GENERATOR_PLATFORM=ARM && shift & goto ArgLoop)
-if /i [%1] == [x86] (set BUILD_ARCH=x86 && set BUILD_CMAKE_GENERATOR_PLATFORM=Win32 && shift & goto ArgLoop)
-if /i [%1] == [--lib] (set BUILD_LIB=%2 && shift && shift & goto ArgLoop)
+if /i [%1] == [Release] (set "BUILD_CONFIG=Release" && set "MSVC_RUNTIME=MultiThreaded" && shift & goto ArgLoop)
+if /i [%1] == [Debug] (set "BUILD_CONFIG=Debug" && set "MSVC_RUNTIME=MultiThreadedDebug" && shift & goto ArgLoop)
+if /i [%1] == [x64] (set "BUILD_ARCH=x64" && shift & goto ArgLoop)
+if /i [%1] == [ARM64] (set "BUILD_ARCH=ARM64" && set "BUILD_CMAKE_GENERATOR_PLATFORM=ARM64" && shift & goto ArgLoop)
+if /i [%1] == [ARM] (set "BUILD_ARCH=ARM" && set "BUILD_CMAKE_GENERATOR_PLATFORM=ARM" && shift & goto ArgLoop)
+if /i [%1] == [x86] (set "BUILD_ARCH=x86" && set "BUILD_CMAKE_GENERATOR_PLATFORM=Win32" && shift & goto ArgLoop)
+if /i [%1] == [--lib] (set "BUILD_LIB=%2" && shift && shift & goto ArgLoop)
 shift
 goto ArgLoop
 
 :Build
-set LIB_ROOT=%~dp0%BUILD_LIB%
+set "LIB_ROOT=%~dp0%BUILD_LIB%"
 
 rem Inject override CMakeLists.txt if the lib doesn't have its own
-set OVERRIDE_CMAKE=%~dp0cmake\%BUILD_LIB%\CMakeLists.txt
+set "OVERRIDE_CMAKE=%~dp0cmake\%BUILD_LIB%\CMakeLists.txt"
 if exist "%OVERRIDE_CMAKE%" (
     copy /Y "%OVERRIDE_CMAKE%" "%LIB_ROOT%\CMakeLists.txt" >nul
 )
--- ci-build.cmd
+++ ci-build.cmd
@@ -5,6 +5,6 @@ set "RARCH=%2"
 
 for %%L in (cimgui cimplot cimplot3d cimnodes cimnodes_r cimguizmo cimguizmo_quat cimCTE) do (
     echo === Building %%L ===
-    call %~dp0build-native.cmd %RTYPE% %RARCH% --lib %%L
+    call "%~dp0build-native.cmd" "%RTYPE%" "%RARCH%" --lib "%%L"
     if errorlevel 1 exit /b 1
 )
